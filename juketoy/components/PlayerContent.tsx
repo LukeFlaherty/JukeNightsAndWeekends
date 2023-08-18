dfbac0042d8f29f3e50209736b6a0366f5024221
Luke <flaherty.luke@yahoo.com>
2023-08-17 23:36:05 -0400

    adding the player now but farming commits lol

---
+++ juketoy/components/PlayerContent.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const PlayerContent = () => {
+  return <div>Player Content</div>;
+};
+
+export default PlayerContent;
---
+++ juketoy/hooks/useOnPlay.ts
@@ -0,0 +1,24 @@
+import { Song } from "@/types";
+import useAuthModal from "./useAuthModal";
+import { useUser } from "./useUser";
+import usePlayer from "./usePlayer";
+
+const useOnPlay = (songs: Song[]) => {
+  const player = usePlayer();
+  const authModal = useAuthModal();
+  const { user } = useUser();
+
+  const onPlay = (id: string) => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    // play song
+    player.setId(id);
+    // create "playlist" of next songs
+    player.setIds(songs.map((song) => song.id));
+  };
+  return onPlay;
+};
+
+export default useOnPlay;
