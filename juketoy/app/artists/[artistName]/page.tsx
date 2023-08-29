8a06c419b07c5bdeb0be014594010fd10b17d136
Luke <flaherty.luke@yahoo.com>
2023-08-29 18:15:00 -0400

    I think this is the approach

--- juketoy/app/artists/[artistName]/page.tsx
+++ juketoy/app/artists/[artistName]/page.tsx
@@ -6,25 +6,21 @@ import getArtistDetails from "@/actions/getArtistDetails";
 import getArtistSongs from "@/actions/getArtistSongs";
 import getAllArtists from "@/actions/getAllArtists";
 
-interface ArtistDetailProps {
-  params: { artistID: string };
-}
-
-const ArtistDetail: FC<ArtistDetailProps> = async ({ params }) => {
-  const artistDetails = await getArtistDetails(params.artistID);
+const ArtistDetail = async () => {
+  // const artistDetails = await getArtistDetails(params.artistID);
   // artist name, description, profile_image, uuid
-  const artistSongs = await getArtistSongs(params.artistID);
+  // const artistSongs = await getArtistSongs(params.artistID);
   // artistID: uuid, songId, title, duration, url, album, artist, albumImage
   const artists = (await getAllArtists()) || [];
 
-  console.log("WHATEVER THIS IS:", params.artistID);
+  // console.log("WHATEVER THIS IS:", params.artistID);
 
   return (
     <div className="bg-lightModeBackground rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <h1 className="text-mainBrandColor text-3xl font-semibold">
-        Songs by {params.artistID}
+        Songs by {artists[2].name}
       </h1>
-      <ArtistContent songs={artistSongs || []} />
+      {/* <ArtistContent songs={artistSongs || []} /> */}
     </div>
   );
 };
