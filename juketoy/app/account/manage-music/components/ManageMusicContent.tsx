765206c0fee325fd146c1177d3558b77192c0a6c
Luke <flaherty.luke@yahoo.com>
2023-09-09 02:41:23 -0400

    fed info into manage dashboard

---
+++ juketoy/app/account/manage-music/components/ManageMusicContent.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { FC } from "react";
+import { Song } from "@/types";
+import MediaItem from "@/components/MediaItem";
+import LikeButton from "@/components/LikeButton";
+import useOnPlay from "@/hooks/useOnPlay";
+import { useUser } from "@/hooks/useUser";
+import { useRouter } from "next/navigation";
+
+const ManageMusicContent = () => {
+  const router = useRouter();
+  const {
+    isLoading,
+    userDetails,
+    artistDetails,
+    fetchUserDetails,
+    fetchArtistDetails,
+  } = useUser();
+
+  return (
+    <div className="flex flex-col gap-y-2 w-full p-6">
+      <span>
+        <strong>Artist:</strong> {artistDetails!.name}
+      </span>
+      {/* Artist details */}
+      <div className="mt-10">
+        <div className="flex flex-col md:flex-row items-center gap-x-5">
+          <div className="relative h-32 w-32 lg:h-44 lg:w-44">
+            <img
+              className="object-cover"
+              src={userDetails?.avatar_url || "/images/default_artist.png"}
+            />
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <div className="flex flex-col gap-y-1">
+              <h1 className="text-3xl font-semibold">Manage Music</h1>
+              <p className="text-sm">
+                This is a platform for talented artists like you. Use the form
+                below to upload your music. We support MP3 format for songs and
+                any common image format for the cover image.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Songs */}
+    </div>
+  );
+};
+
+export default ManageMusicContent;
--- juketoy/app/account/manage-music/page.tsx
+++ juketoy/app/account/manage-music/page.tsx
@@ -1,16 +1,60 @@
-import { FC } from "react";
+import Header from "@/components/Header";
+import ProtectArtistRoute from "./components/ProtectArtistRoute";
+import getArtistDetails from "@/actions/getArtistDetails";
+import getArtistSongs from "@/actions/getArtistSongs";
+import ManageMusicContent from "./components/ManageMusicContent";
 
-const ManageMusic: FC = () => {
+const ManageMusic = () => {
+  const handleEditSong = (songId: string) => {
+    // Implement your edit song logic here
+    console.log(`Editing song with ID: ${songId}`);
+  };
+
+  const handleDeleteSong = (songId: string) => {
+    // Implement your delete song logic here
+    console.log(`Deleting song with ID: ${songId}`);
+  };
+
+  //       const artistDetails = await getArtistDetails(artistId);
+  //   const artistSongs = await getArtistSongs(artistDetails!.artist_id);
+
   return (
-    <div className="bg-lightModeBackground rounded-lg h-full w-full overflow-hidden overflow-y-auto">
-      <div className="p-4">
-        <h1 className="text-2xl font-bold mb-4 text-mainBrandColor">
-          Manage Music
-        </h1>
-        <p>This is a placeholder page for managing your music as an artist.</p>
-        {/* Add your manage music functionality here */}
+    <ProtectArtistRoute>
+      <div className="rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+        <Header>
+          <h1 className="text-2xl font-bold mb-4">Manage Your Music</h1>
+          <p className="mb-8">
+            Welcome to the Music Management Portal, where you can take control
+            of your artistic journey. Here, you can manage and organize your
+            music collection effortlessly. Below, you'll find tools and options
+            to handle your songs and albums. Here's what you can do:
+          </p>
+          <ul className="list-disc ml-6 mb-8">
+            <li>
+              <strong>Upload Music:</strong> Share your latest tracks with your
+              audience by uploading your songs in MP3 format.
+            </li>
+            <li>
+              <strong>Edit Song Details:</strong> Customize song titles,
+              descriptions, and cover art to make your music stand out.
+            </li>
+            <li>
+              <strong>Manage Playlists:</strong> Organize your songs into
+              playlists for a seamless listening experience.
+            </li>
+            <li>
+              <strong>Delete Songs:</strong> Remove songs that are no longer
+              part of your repertoire.
+            </li>
+          </ul>
+          <p>
+            Feel free to explore and manage your music library. We're here to
+            support your artistic journey!
+          </p>
+          <ManageMusicContent />
+        </Header>
       </div>
-    </div>
+    </ProtectArtistRoute>
   );
 };
 
